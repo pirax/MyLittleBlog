6d35cc71ba27767b8dc30e314e60bdf5cbaef611
Marcin Sztolcman <marcin@urzenia.net>
2011-04-14 16:09:30 +0200

    formularz dodawania wpisu

---
+++ admin/action_add.php
@@ -0,0 +1,5 @@
+<form method="post">
+<label for="subject"><input type="text" name="subject" id="subject" /></label>
+<label for="content"><textarea name="content" id="content"></textarea></label>
+<input type="submit" name="save" value="Zapisz" />
+</form>

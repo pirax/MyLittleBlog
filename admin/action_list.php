74188f39de39bc5095bb0c9010e957b6aef1f08c
Marcin Sztolcman <marcin@urzenia.net>
2011-04-15 13:23:19 +0200

    wstepny widok z lista artykulow

--- admin/action_list.php
+++ admin/action_list.php
@@ -1,4 +1,29 @@
+<table width="100%">
+<thead>
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</thead>
+<tbody>
 <?php
 
-print_r (entry_list ());
+$entries = entry_list ();
 
+foreach ($entries as $entry) {
+    printf ("
+    <tr>
+        <td>%d</td>
+        <td>%s</td>
+        <td><a href='/?action=entry_edit&slug=%s'>edytuj</a></td>
+    </tr>",
+        $i,
+        $entry['subject'],
+        $entry['slug'],
+    );
+}
+
+?>
+</tbody>
+</table>

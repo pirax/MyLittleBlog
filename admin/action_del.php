0157efb9b0c08b3052a4e9c5fa1b76cd3eb6bdeb
Marcin Sztolcman <marcin@urzenia.net>
2011-05-11 04:56:04 +0200

    dodana akcja do usuwania wpisow

---
+++ admin/action_del.php
@@ -0,0 +1,25 @@
+<?php
+
+if (!isset ($_GET['slug'])) {
+    $_SESSION['msg'] = 'Brak identyfikatora wpisu';
+    header ('Location: /admin/');
+    exit;
+}
+
+$path = slug_to_path ($_GET['slug']);
+if (!file_exists ($path)) {
+    $_SESSION['msg'] = 'Nie znaleziono wpisu o identyfikatorze "'. html ($_GET['slug']) .'"';
+    header ('Location: /admin/');
+    exit;
+}
+
+if (@unlink ($path)) {
+    $_SESSION['msg'] = 'Wpis o identyfikatorze "'. html ($_GET['slug']) .'" został usunięty';
+}
+else {
+    $_SESSION['msg'] = 'Nie udało się usunąć wpisu o identyfikatorze "'. html ($_GET['slug']) .'"';
+}
+
+header ('Location: /admin/');
+exit;
+
